1afb16edd0da48c7700faa31dbf1b429517703c2
unknown <wkevg12@gmail.com>
2020-06-07 19:22:47 -0400

    Updates

---
+++ precache-manifest.2d7f4595413fbdf1edbf8235f0c55723.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f96b19e89cc02a127e2c920cdbc796bd",
+    "url": "/portfolio_website/index.html"
+  },
+  {
+    "revision": "3d1754ba1a2a5d8cf334",
+    "url": "/portfolio_website/static/css/2.eb43dee1.chunk.css"
+  },
+  {
+    "revision": "ea543f354976261d2c8e",
+    "url": "/portfolio_website/static/css/main.91a32938.chunk.css"
+  },
+  {
+    "revision": "3d1754ba1a2a5d8cf334",
+    "url": "/portfolio_website/static/js/2.d3c63cbe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio_website/static/js/2.d3c63cbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea543f354976261d2c8e",
+    "url": "/portfolio_website/static/js/main.8303583f.chunk.js"
+  },
+  {
+    "revision": "41ce8dfe7453f144fc6f",
+    "url": "/portfolio_website/static/js/runtime-main.18ac2dcb.js"
+  },
+  {
+    "revision": "2931207b3034f8b8fc212dce29bea382",
+    "url": "/portfolio_website/static/media/animat-coffee.2931207b.gif"
+  },
+  {
+    "revision": "87b1d2cf9fd22c4ab2ef553e27574607",
+    "url": "/portfolio_website/static/media/appDemo.87b1d2cf.gif"
+  },
+  {
+    "revision": "c24dfc179710b57c79ab2aa4c90b2ee5",
+    "url": "/portfolio_website/static/media/demoapp2.c24dfc17.gif"
+  },
+  {
+    "revision": "07bb69e3ce6ba6cb15bf6c8ccdf8af09",
+    "url": "/portfolio_website/static/media/typing.07bb69e3.gif"
+  }
+]);
